10a0616d35cb7a8f2549cdc76be6bdc61da91851
yn303-17 <yn303-17@DESKTOP-B9POVOT>
2020-08-11 14:55:33 +0900

    자바 1일차 예제

---
+++ doitjava/src/doitjava/MyName.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+//한 줄 주석입니다.
+//이것은 주석이 아니라서 예외(에러)를 발생시킵니다.
+/*한 번에 많은 주석을 넣을 때는 이 주석을 사용합니다.*/
+/*
+	
+	여러 줄에
+	주석을 적으려면
+	이 주석을
+	사용합니다.
+
+*/
+package doitjava;
+
+public class MyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("제 이름은 서준호입니다."); //화면 출력 코드
+		System.out.println("직업은 강사입니다."); //둘째 줄의 출력 코드
+	}
+	/*
+	 * 첫 수업 시간에 작성한 코드입니다.
+	 * 어떤 언엉를 학습할 떄 최초로 화면 출력을
+	 * 배우게 됩니다.
+	 * */
+}
