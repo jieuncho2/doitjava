9e609e0b9eec9d5929442ee888dd1f816ceab57d
yn303-17 <yn303-17@DESKTOP-B9POVOT>
2020-08-13 13:43:51 +0900

    test

---
+++ doitjava/src/chapter02/Variable.java
@@ -0,0 +1,19 @@
+package chapter02;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int level = 10;
+		int hp;
+		System.out.println("현재 레벨은 얼마인가요?");
+		System.out.println(level);
+		hp = 500;
+		System.out.println("현재 체력 게이지는 얼마인가요?");
+		System.out.println(hp);
+		System.out.println("적으로부터 100의 데미지를 입었습니다.");
+		hp =400;
+		System.out.println("현재 체력 게이지는 얼마인가요?");
+		System.out.println(hp);
+	}
+
+}
