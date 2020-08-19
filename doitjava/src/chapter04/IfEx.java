dcbcb56f047f6fd77ea03721429a8a7f76284ea4
jieuncho2 <jieunchoi.developer@gmail.com>
2020-08-19 16:03:54 +0900

    revise

--- doitjava/src/chapter04/IfEx.java
+++ doitjava/src/chapter04/IfEx.java
@@ -8,7 +8,7 @@ public class IfEx {
 		if (score >= 90) {
 			System.out.println("점수가 90보다 크거나 같습니다.");
 			System.out.println("등급은 A입니다.");
-		} else {// else 구문은 if문에서 false일 경우 실행되는 영역
+		} else {	// else 구문은 if문에서 false일 경우 실행되는 영역
 			System.out.println("점수가 90보다 작습니다.");
 			System.out.println("등급은 B입니다.");
 		}
