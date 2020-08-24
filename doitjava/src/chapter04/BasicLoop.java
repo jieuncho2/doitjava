b1308e2bac061a418186e1a75a0e5309de671d9f
jieuncho2 <jieunchoi.developer@gmail.com>
2020-08-24 13:11:28 +0900

    chapter04 - do while문

--- doitjava/src/chapter04/BasicLoop.java
+++ doitjava/src/chapter04/BasicLoop.java
@@ -15,6 +15,29 @@ public class BasicLoop {
 		num += 10;
 
 		System.out.println("1부터 10까지의 합은 " + num + "입니다.");
+		
+		int cnt = 1; // count의 줄임
+		int res = 0; // result의 줄임
+		res += cnt++; // 1
+		res += cnt++; // 2
+		res += cnt++; // 3
+		res += cnt++; // 4
+		res += cnt++; // 5
+		res += cnt++; // 6
+		res += cnt++; // 7
+		res += cnt++; // 8
+		res += cnt++; // 9
+		res += cnt++; // 10
+		
+		System.out.println("1부터 10까지의 합은 " + res + "입니다.");
+		
+		cnt = 1;
+		res = 0;
+		while(cnt <= 10) {
+			res += cnt++;
+		}
+		
+		System.out.println("1부터 10까지의 합은 " + res + "입니다.");
 	}
 
 }
