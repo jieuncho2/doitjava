b4802215f4e6f708d89fa154dfdd057e3813132f
jieuncho2 <jieunchoi.developer@gmail.com>
2020-08-25 20:07:36 +0900

    chapter04 - Exercise2

--- doitjava/src/chapter04/Z123p2.java
+++ doitjava/src/chapter04/Z123p2.java
@@ -4,7 +4,18 @@ public class Z123p2 {
 
 	public static void main(String[] args) {
 		
+		int dan;
+		int times;
+		int res;
 		
+		for(dan=2; dan<=9; dan++) {
+			if(dan%2 !=0) continue;
+			for(times=1; times<=9; times++) {
+				res = dan * times;
+				System.out.println(dan + "X" + times + "=" + res);
+			}
+			System.out.println("홀수단입니다");
+		}
 		
 	}
 
