1d7c67daba1bda0fc99148f4460a6472cacfb74d
jieuncho2 <jieunchoi.developer@gmail.com>
2020-08-25 20:31:19 +0900

    chapter04 - Exercise4,5

---
+++ doitjava/src/chapter04/Z123p4.java
@@ -0,0 +1,28 @@
+package chapter04;
+
+public class Z123p4 {
+
+	public static void main(String[] args) {
+		
+		int lineCount = 4;  
+		int spaceCount = lineCount/2 +1;
+		int starCount = 1;
+		
+		for(int i = 0; i<lineCount; i++) {
+			for(int j = 0; j<spaceCount; j++) {
+				System.out.print(' ');
+			}
+			for(int j=0; j<starCount; j++) {
+				System.out.print('*');
+			}
+			for(int j = 0; j<spaceCount; j++) {
+				System.out.print(' ');
+			}
+			spaceCount-=1;
+			starCount+=2;
+			System.out.println();
+		}
+		
+	}
+
+}
