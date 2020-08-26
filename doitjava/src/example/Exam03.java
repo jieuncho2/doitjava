66d5121dbbf72a4215a8b2b4ae41ce6e169a3cf5
jieuncho2 <jieunchoi.developer@gmail.com>
2020-08-26 14:48:31 +0900

    Exercise

--- doitjava/src/example/Exam03.java
+++ doitjava/src/example/Exam03.java
@@ -10,13 +10,13 @@ public class Exam03 {
 		while(cnt<=10) {
 			res += cnt++;
 		}
-		System.out.println("1부터 10까지의 합은" + res + "입니다");
+		System.out.println("1부터 10까지의 합은 " + res + "입니다");
 		
 		int sum = 0; // T
 		for(int i=1; i<=10; i++) {
 			sum += i;
 		}
-		System.out.println("1부터 10까지의 합은" +sum + "입니다");
+		System.out.println("1부터 10까지의 합은 " +sum + "입니다");
 	}
 
 }
---
+++ doitjava/src/example/Exam04.java
@@ -0,0 +1,17 @@
+package example;
+
+import java.util.Scanner;
+
+public class Exam04 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i=1; i<=10; i++) {
+			if(i%2==0) {
+				sum += i;			
+			}
+		}
+		System.out.println("1부터 10까지의 수 중 짝수의 합은 " +sum + "입니다");
+	}
+
+}
