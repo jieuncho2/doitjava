853affa74b0956ad2b54a2aff44ae346d4929a63
jieuncho2 <jieunchoi.developer@gmail.com>
2020-08-26 15:00:10 +0900

    Exercise

--- doitjava/src/example/Exam03.java
+++ doitjava/src/example/Exam03.java
@@ -16,7 +16,7 @@ public class Exam03 {
 		for(int i=1; i<=10; i++) {
 			sum += i;
 		}
-		System.out.println("1부터 10까지의 합은 " +sum + "입니다");
+		System.out.println("1부터 10까지의 합은 " + sum + "입니다");
 	}
 
 }
--- doitjava/src/example/Exam04.java
+++ doitjava/src/example/Exam04.java
@@ -11,7 +11,16 @@ public class Exam04 {
 				sum += i;			
 			}
 		}
-		System.out.println("1부터 10까지의 수 중 짝수의 합은 " +sum + "입니다");
+		System.out.println("1부터 10까지의 수 중 짝수의 합은 " + sum + "입니다");
+		
+		int sum1 = 0;
+		for(int i=1; i<=10; i++) {
+			if(i%2!=0) {
+				continue;			
+			}
+			sum1 += i;
+		}
+		System.out.println("1부터 10까지의 수 중 짝수의 합은 " + sum1 + "입니다");
 	}
 
 }
