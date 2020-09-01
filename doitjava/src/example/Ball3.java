4f87ff24188e43fb7c7fa616d9432e022f98425e
jieuncho2 <jieunchoi.developer@gmail.com>
2020-09-01 16:06:42 +0900

    Exercise

---
+++ doitjava/src/example/Ball3.java
@@ -0,0 +1,36 @@
+package example;
+
+import java.util.Scanner;
+
+public class Ball3 {
+
+	public static void main(String[] args) {
+		//달의 마지막 일자를 출력하는 프로그램 작성
+		//switch case 구문을 사용
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("몇 월?");
+		System.out.print(" > ");
+		int thisMonth = sc.nextInt();
+		int day=0;
+		
+		switch(thisMonth) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 9:
+		case 11:
+			day = 31;
+			break;
+		case 2:
+			day = 28;
+			break;
+		default:
+			day = 30;
+		}
+		System.out.println(thisMonth + "월은 " + day + "일까지입니다.");
+		sc.close();
+	}
+}
---
+++ doitjava/src/example/Ball4.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.util.Scanner;
+
+public class Ball4 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 10;
+		for(i=2; i<n; i++) {
+			for() {
+				cnt++;
+			}
+		}
+	}
+}
