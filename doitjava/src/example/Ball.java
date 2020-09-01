9ca56b1f7a9ad26d8962cae4e8afafa0e5f97978
jieuncho2 <jieunchoi.developer@gmail.com>
2020-09-01 12:07:48 +0900

    Exercise

---
+++ doitjava/src/example/Ball.java
@@ -0,0 +1,30 @@
+package example;
+
+import java.util.Scanner;
+
+public class Ball {
+
+	public static void main(String[] args) {
+		int user, com;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			com = (int)(Math.random()*100);
+			System.out.println("업다운 게임입니다.");
+			System.out.println("1부터 100까지 숫자 중 생성된 숫자는 무엇인가요?");
+			System.out.print("> ");
+			user = sc.nextInt();
+			
+			if(user==com) {
+				System.out.println("정답");
+			} else if(user<com) {
+				System.out.println("up");
+			} else if(user>com){
+				System.out.println("down");
+			} else {
+				System.out.println("오류");
+			}
+		}
+	}
+}
