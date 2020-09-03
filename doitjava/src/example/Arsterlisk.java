9aceac9707640156c86b7e1972d20d424e5d52dc
jieuncho2 <jieunchoi.developer@gmail.com>
2020-09-03 13:44:19 +0900

    Arsterlisk

---
+++ doitjava/src/example/Arsterlisk.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Arsterlisk {
+
+	public static void main(String[] args) {
+		for(int i=0; i<3; i++) {	//	row
+			for(int j=0; j<8; j++) {	//col
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+}
---
+++ doitjava/src/example/Arsterlisk2.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Arsterlisk2 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {	//	row
+			for(int j=0; j<=i; j++) {	//col
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
