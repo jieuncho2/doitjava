916c831d24a3829c475d6866ac575158e7fc10bb
jieuncho2 <jieunchoi.developer@gmail.com>
2020-09-03 14:32:49 +0900

    Exercise

--- doitjava/src/example/Ma.java
+++ doitjava/src/example/Ma.java
@@ -5,7 +5,7 @@ public class Ma {
 	public static void main(String[] args) {
 		for(int i=4; i>=0; i--) {	//	row
 			for(int j=0; j<i; j++) {
-				System.out.print("@");
+				System.out.print(" ");
 			}
 			System.out.println(i+1);
 		}
