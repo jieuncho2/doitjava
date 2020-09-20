a64787ef127926fafca146cf7062d2c6e119c82c
jieuncho2 <jieunchoi.developer@gmail.com>
2020-09-20 13:01:27 +0900

    revise

--- doitjava/src/chapter08/AnimalEx.java
+++ doitjava/src/chapter08/AnimalEx.java
@@ -1,6 +1,6 @@
 package chapter08;
 
-public class AnimalEx {
+public abstract class AnimalEx {
 
 	public static void main(String[] args) {
 		Dog dog = new Dog();
@@ -9,12 +9,14 @@ public class AnimalEx {
 		cat.sound();
 		System.out.println("------------");
 		
+		//변수의 자동 타입 변환
 		Animal animal = null;
 		animal = new Dog();
 		animal.sound();
 		animal = new Cat();
 		animal.sound();
 		System.out.println("------------");
+		//메소드의 다형성
 		
 		animalSound(new Dog());
 		animalSound(new Cat());
