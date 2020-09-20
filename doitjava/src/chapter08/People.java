e439ad1053a4d34502ac7e541435bc17250839ca
jieuncho2 <jieunchoi.developer@gmail.com>
2020-09-20 11:45:13 +0900

    revise

--- doitjava/src/chapter08/People.java
+++ doitjava/src/chapter08/People.java
@@ -1,10 +1,11 @@
 package chapter08;
 
 public class People {
+	//필드
 	public String name;
 	public String ssn;
 	
-	
+	//생성자
 	public People(String name, String ssn) {
 		this.name=name;
 		this.ssn=ssn;
