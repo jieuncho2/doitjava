b4008675eb309577e8b0a34cd788530ea2726b3e
jieuncho1 <jieunchoi.developer@gmail.com>
2020-09-21 14:59:46 +0900

    chapter12

--- doitjava/src/chapter12/Util.java
+++ doitjava/src/chapter12/Util.java
@@ -12,4 +12,10 @@ public class Util {
 		boolean valueCompare = p1.getKey().equals(p2.getKey());
 		return keyCompare && valueCompare;
 	}
+	
+	public static <T extends Number> int comapare(T t1, T t2) {
+		double v1 = t1.doubleValue();
+		double v2 = t2.doubleValue();
+		return Double.compare(v1, v2);
+	}
 }
